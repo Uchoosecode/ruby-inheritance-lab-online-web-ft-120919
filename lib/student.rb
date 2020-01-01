25df0aa36aebaf54a2b2f23e76689301cf103749
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-01 18:54:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student < User
 
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name= "Steve", last_name= "")
+  def initialize(first_name= "Steve", last_name= "Jobs")
     @first_name = first_name
     @last_name = last_name
   end
